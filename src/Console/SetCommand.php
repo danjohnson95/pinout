e8a3ef9af9c5ead6a708ffc85f0fb85fbb5673ba
Dan Johnson <dan@try.be>
2024-02-21 20:43:04 +0000

    feat: add command for setting

---
+++ src/Console/SetCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DanJohnson95\Pinout\Console;
+
+use DanJohnson95\Pinout\Enums\Func;
+use DanJohnson95\Pinout\Enums\Level;
+use DanJohnson95\Pinout\Facade as Pin;
+use Illuminate\Console\Command;
+
+class SetCommand extends Command
+{
+    protected $signature = 'pinout:set {pin} {--func} {--level}';
+    protected $description = 'Sets the state of the pin';
+
+    public function handle()
+    {
+        $pinNumber = $this->argument('pin');
+
+        if (! is_numeric($pinNumber)) {
+            $this->error('Pin must be a number');
+            return 1;
+        }
+
+        // Check the options are valid first...
+
+        if ($func = $this->option('func')) {
+            $func = Func::tryFrom($func);
+        }
+
+        if ($level = $this->option('level')) {
+            $level = Level::tryFrom($level);
+        }
+
+        // Now set them.
+        if ($func) {
+            Pin::setFunction($pinNumber, $func);
+        }
+
+        if ($level) {
+            Pin::setLevel($pinNumber, $level);
+        }
+
+        $state = Pin::get($this->argument('pin'));
+
+        $this->info("Pin {$state->pin} is currently {$level}");
+    }
+}

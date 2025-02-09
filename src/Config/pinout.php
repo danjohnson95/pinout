5d4949d9954069313aff56eefb810a1f92582ddf
Dan Johnson <dan@try.be>
2025-02-09 16:22:26 +0000

    feat: add config file

---
+++ src/Config/pinout.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /**
+     * Here you must define which GPIO pins your application is going to use,
+     * and the direction each one will have (input or output).
+     */
+    'pins' => [
+        // For example,
+        // 1 => Direction::OUTPUT,
+        // 2 => Direction::INPUT,
+    ],
+];

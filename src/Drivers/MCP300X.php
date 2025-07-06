8a7726b3dca63ac9f2e851951627183841789408
benammi <benammiswift@gmail.com>
2025-07-06 15:16:52 +0100

    feature: added the MCP300X device

---
+++ src/Drivers/MCP300X.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DanJohnson95\Pinout\Drivers;
+
+use DanJohnson95\Pinout\Entities\Pin;
+use DanJohnson95\Pinout\Enums\SPIMode;
+use DanJohnson95\Pinout\Enums\MCP;
+use DanJohnson95\Pinout\Devices\SPIBus;
+
+class MCP300X
+{
+    private int $maxPin = 7;
+    private int $mcp3004MaxPin = 3;
+    private int $maxVoltageInt = 1023
+
+    public static function make (
+        Pin $chipSelect,
+        Pin $clock,
+        Pin $dataIn,
+        Pin $dataOut,
+        MCP $model,
+        float $vcc
+    ): self {
+
+        return new self(
+            spi: app()->makeWith([
+                'chipSelect' => $chipSelect,
+                'clock' => $clock,
+                'dataIn' => $dataIn,
+                'dataOut' => $dataOut,
+                'mode' => SPIMode::MODE0
+            ]),
+            vcc: $vcc,
+            model: $model
+        );
+        
+        return (new self(
+            $chipSelect,
+            $clock,
+            $dataIn,
+            $dataOut,
+            $mode
+        ))->init();
+    }
+
+    private function __construct (
+        protected ?SPIBus $spi,
+        protected float $vcc,
+        protected MCP $model
+    ) {
+        //
+    }
+
+    public function getAnalogPin(
+        int $pinNumber
+    ): ?float {
+        switch (true) {
+            case ($pinNumber > $this->maxPin):
+            case ($pinNumber < 0):
+                throw new \Exception("MCP300X error : Pin $pinNumber is not a valid pin");
+                break;
+
+            case ($pinNumber < $this->mcp3004MaxPin):
+                throw new \Exception("MCP300x error : Pin $pinNumber is not a valid pin on MCP3004");
+                break;
+
+            default:
+                break;
+        }
+
+        $this->spi
+            ->enableChip()
+            ->writeBits(
+                sprintf('000000011%03b000000000000', $pinNumber),
+                true
+            )->disableChip();
+
+        $data = substr($spi->readBits, -10);
+        $analogLevel = bindec($data);
+        return ($this->vcc / $this->maxVoltageInt) * $analogLevel;
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace DanJohnson95\Pinout;
 
 use DanJohnson95\Pinout\Shell\Commandable;
+use DanJohnson95\Pinout\Devices\SPIBus;
 use DanJohnson95\Pinout\Shell\SysFile;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
@@ -16,6 +17,15 @@ class ServiceProvider extends BaseServiceProvider
         );
 
         $this->app->bind(Commandable::class, config('pinout.sys_file'));
+        $this->app->bind(SPIBus::class, function ($app, $parameters) {
+            return SPIBus::make(
+                chipSelect: $parameters['chipSelect'],
+                clock: $parameters['clock'],
+                dataIn: $parameters['dataIn'],
+                dataOut: $parameters['dataOut'],
+                mode: $parameters['mode']
+            );
+        });
     }
 
     public function boot()
